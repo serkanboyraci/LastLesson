6192ca67d0dd0b84f6788d246ef651582c6ec1ce
serkanboyraci <serkanboyraci@gmail.com>
2023-01-28 00:52:17 +0300

    updated

---
+++ List/ListModel.swift
@@ -0,0 +1,15 @@
+//
+//  ListModel.swift
+//  LastLesson
+//
+//  Created by Ali serkan Boyracı  on 14.01.2023.
+//
+
+import Foundation
+
+protocol ListModelProtocol: AnyObject {
+    
+    func didDataFetch()
+}
+
+class ListModel
---
+++ List/ListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewModel.swift
+//  LastLesson
+//
+//  Created by Ali serkan Boyracı  on 14.01.2023.
+//
+
+import Foundation
+
+class ListViewModel {
+    
+    func didViewLoad() {
+        
+    }
+    
+    func itemPressed(_ index: Int) {
+        
+    }
+}
